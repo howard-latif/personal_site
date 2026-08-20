2546b9016a0d2a49dd28693ac7e516d673649d1b
howard-latif <howard.latif@proton.me>
2026-08-20 18:33:33 +0200

    one

--- file-routes.d.ts
+++ file-routes.d.ts
@@ -61,6 +61,12 @@ declare module "virtual:file-routes" {
       page: true;
       $component: FileRouteLazyRef<typeof import("./src/routes/personal_site/projects")>;
       $$route?: undefined;
+    },
+    {
+      path: "/personal_site/One";
+      page: true;
+      $component: FileRouteLazyRef<typeof import("./src/routes/personal_site/One")>;
+      $$route?: undefined;
     }
   ];
   export default routes;
@@ -80,6 +86,14 @@ declare module "virtual:file-routes" {
       id: "/personal_site/";
       page: true;
       $component: FileRouteLazyRef<typeof import("./src/routes/personal_site/index")>;
+      $$route?: undefined;
+      children?: undefined;
+    },
+    {
+      path: "/personal_site/One";
+      id: "/personal_site/One";
+      page: true;
+      $component: FileRouteLazyRef<typeof import("./src/routes/personal_site/One")>;
       $$route?: undefined;
       children?: undefined;
     },
---
+++ src/routes/personal_site/One.tsx
@@ -0,0 +1,13 @@
+export default function One() {
+  return (
+    <div class="center">
+      <figure style="margin: 1em;">
+        <figure style="margin: 0.5em;">
+          <figure style="margin: 0.15em; padding: 1em;">
+            <img class="landing-img noselect" src="/personal_site/one.jpg" />
+          </figure>
+        </figure>
+      </figure>
+    </div>
+  );
+}
